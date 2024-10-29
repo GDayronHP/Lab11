9767c017c37719559ab69c13b389b19d99ae43f0
Joseph <geometryhuayra@gmail.com>
2024-10-29 14:27:58 -0500

    Merge with Maxito

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -1,8 +1,5 @@
 package com.tecsup.petclinic.services;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +9,47 @@ import com.tecsup.petclinic.exception.VetNotFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @Slf4j
 public class VetServiceTest {
 
     @Autowired
     private VetService vetService;
+
+    @Test
+    public void testUpdateVet() {
+
+        String VET_NAME = "James";
+        String LAST_NAME = "Carter";
+
+        String UP_VET_NAME = "James editado";
+        String UP_VET_LAST_NAME = "Carter editado";
+
+        Vet vet = new Vet(VET_NAME, LAST_NAME);
+
+        // ------------ Create ---------------
+
+        log.info(">" + vet);
+        Vet vetCreated = this.vetService.create(vet);
+        log.info(">>" + vetCreated);
+
+        // ------------ Update ---------------
+
+        // Prepare data for update
+        vetCreated.setFirstName(UP_VET_NAME);
+        vetCreated.setLastName(UP_VET_LAST_NAME);
+
+        // Execute update
+        Vet upgradeVet = this.vetService.update(vetCreated);
+        log.info(">>>>" + upgradeVet);
+
+        //            EXPECTED        ACTUAL
+        assertEquals(UP_VET_NAME, upgradeVet.getFirstName());
+        assertEquals(UP_VET_LAST_NAME, upgradeVet.getLastName());
+    }
 
     @Test
     public void testDeleteVet() {
